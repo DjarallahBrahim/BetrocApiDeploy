16378e3e0c2eb99f4c26e9eac4f8a5df9932c6a8
root <root@vps628622.ovh.net>
2019-01-14 22:03:47 +0100

    updtae default page size of getAD

--- src/main/java/com/betroc/controller/AdBaseController.java
+++ src/main/java/com/betroc/controller/AdBaseController.java
@@ -22,7 +22,7 @@ public abstract class AdBaseController <T extends Advertisement,W extends Advert
 
     @GetMapping
     //@Secured("ROLE_USER")
-    public Page getAllAds(@PageableDefault(size = 10, sort = "id") Pageable pageable){
+    public Page getAllAds(@PageableDefault(size = 20, sort = "id") Pageable pageable){
 
         return repository.findAll(pageable);
     }
